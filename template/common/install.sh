3e3c28bd98cb6cc2ed3d1416fa411330b822ffba
chickendrop89 <chickendrop89@gmail.com>
2024-10-13 15:56:31 +0200

    slg: template: install: trigger setup wizard after reboot
    Signed-off-by: chickendrop89 <chickendrop89@gmail.com>

--- template/common/install.sh
+++ template/common/install.sh
@@ -1,3 +1,6 @@
+SETUP_WIZARD_INTENT="com.google.android.setupwizard"
+SETUP_WIZARD_INTENT="$SETUP_WIZARD_INTENT/$SETUP_WIZARD_INTENT.SetupWizardActivity"
+
 wipe_cache(){
     if [[ -e "/data/system/package_cache" ]]; 
       then
@@ -9,5 +12,15 @@ wipe_cache(){
     fi
 }
 
-ui_print "- Wiping cache to prevent weird behaviour"
+ui_print "- Wiping cache to prevent undefined behaviour"
 wipe_cache
+
+trigger_setup_wizard(){
+    pm enable --user 0 "$SETUP_WIZARD_INTENT"
+    settings put secure user_setup_complete 0
+    settings put global device_provisioned 0
+}
+
+ui_print "- Setup wizard will be triggered on next boot"
+ui_print "- Rebooting is recommended after the installation completes"
+trigger_setup_wizard
