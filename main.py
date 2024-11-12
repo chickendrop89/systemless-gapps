2bbbd3cfa6382c280beea5c417fa33f3fc8e9e53
chickendrop89 <chickendrop89@gmail.com>
2024-11-12 15:29:37 +0100

    slg: main: refactor  the __preventDuplicateSystem() function
    Signed-off-by: chickendrop89 <chickendrop89@gmail.com>

--- main.py
+++ main.py
@@ -162,29 +162,21 @@ def resolveGappsDirectories():
 
     def __preventDuplicateSystem():
         """
-            Pixel DPS sub-package (DevicePersonalizationServices.{zip, tar.gz})
-            creates duplicate system directory (/system/system)
-            Fix it, and prevent it from happening for other sub-packages as well
+        Prevents *MODULE DIR*/system/system from happening
         """
         system_path = os.path.join(appset_path, "system")
 
-        for __dirpath, subdirs, __files in os.walk(system_path):
-            # System subdirectory path (ex: "appset/system/etc")
-            subdirectory = os.path.join(system_path, *subdirs)
+        for subdir in os.listdir(system_path):
+            system_subdir_path = os.path.join(system_path, subdir)
+            appset_subdir_path = os.path.join(appset_path, subdir)
+            shutil.move(system_subdir_path, appset_subdir_path)
 
-            # Parent directory name of the sub-subdirectory (ex: "etc")
-            sub_subdirectory_parent = subdirectory.split("/")[-1]
-
-            # Sub-subdirectory name (ex: "textclassifier")
-            sub_subdirectory = os.listdir(subdirectory)[0]
-
-            shutil.move(
-                f"{subdirectory}/{sub_subdirectory}",
-                f"{appset_path}/{sub_subdirectory_parent}/{sub_subdirectory}"
-            )
-            shutil.rmtree(system_path)
+        shutil.rmtree(system_path)
 
     def __fixDirectories():
+        """
+        Fixes unprocessed directories in some cases
+        """
         for file in os.listdir(dst_file):
             if file.startswith("___"):
                 src_dir = os.path.join(dst_file, file)
